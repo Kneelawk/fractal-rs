6348b065d860318cfd7248b0a9f66468950f10e7
Kneelawk <kneelawk@gmail.com>
2022-08-06 22:36:54 -0700

    Move completely over to liquid-based shader generation

--- src/generator/gpu/shader/mod.rs
+++ src/generator/gpu/shader/mod.rs
@@ -9,12 +9,11 @@ use crate::{
     util::files::debug_dir,
 };
 use anyhow::Context;
-use liquid_core::object;
 use naga::{
-    back, front,
+    front,
     valid::{ValidationFlags, Validator},
 };
-use std::borrow::Cow;
+use std::{borrow::Cow, path::Path};
 use tokio::{fs::File, io::AsyncWriteExt};
 use wgpu::ShaderSource;
 
@@ -36,77 +35,25 @@ pub async fn load_shaders(opts: FractalOpts) -> anyhow::Result<LoadedShaders> {
     //
 
     info!("Loading fragment shader template...");
-    let template_source = loader
+    let frag_str = loader
         .compile_template(ShaderTemplateOpts {
             path: Cow::Borrowed(FRAGMENT_SHADER_PATH),
-            globals: &object!({
-                "opts": object!({
-                    "c_real": opts.c.re,
-                    "c_imag": opts.c.im,
-                    "iterations": opts.iterations,
-                    "mandelbrot": opts.mandelbrot,
-                    "radius_squared": opts.radius_squared,
-                })
-            }),
+            globals: &opts
+                .globals()
+                .context("Error creating globals for fractal options")?,
         })
         .context("Error loading fragment shader template")?;
 
-    info!("Writing filled template...");
-    let path = debug_dir().join("debug_fragment_template.wgsl");
-    let mut file = File::create(&path)
+    info!("Writing fragment shader WGSL to debug file...");
+    let frag_path = debug_dir().join("debug_fragment.wgsl");
+    let mut file = File::create(&frag_path)
         .await
-        .with_context(|| format!("Error opening {:?} for writing", &path))?;
-    file.write_all(template_source.as_bytes())
+        .with_context(|| format!("Error opening {:?} for writing", &frag_path))?;
+    file.write_all(frag_str.as_bytes())
         .await
-        .with_context(|| format!("Error writing to {:?}", &path))?;
+        .with_context(|| format!("Error writing to {:?}", &frag_path))?;
 
-    info!("Loading utility functions...");
-    let mut module = front::wgsl::parse_str(&template_source)
-        .map_err(|e| {
-            anyhow!(
-                "Error in template file: {:?}\n{}",
-                &path,
-                e.emit_to_string(&template_source)
-            )
-        })
-        .context("Error parsing filled WGSL template")?;
-
-    opts.install(&mut module)
-        .context("Error installing fractal options")?;
-
-    info!("Writing module as txt...");
-    let path = debug_dir().join("debug_fragment.txt");
-    let mut file = File::create(&path)
-        .await
-        .with_context(|| format!("Error opening {:?} for writing", &path))?;
-    file.write_all(format!("{:#?}", &module).as_bytes())
-        .await
-        .with_context(|| format!("Error writing to {:?}", &path))?;
-
-    info!("Validating module...");
-    let mut validator = Validator::new(ValidationFlags::all(), Default::default());
-    let module_info = validator
-        .validate(&module)
-        .context("Error while validating filled WGSL template")?;
-
-    info!("Compiling WGSL...");
-    let mut frag_str = String::new();
-    let mut writer =
-        back::wgsl::Writer::new(&mut frag_str, back::wgsl::WriterFlags::EXPLICIT_TYPES);
-    writer
-        .write(&module, &module_info)
-        .context("Error writing validated WGSL to string")?;
-    writer.finish();
-
-    info!("Writing WGSL...");
-    let path = debug_dir().join("debug_fragment.wgsl");
-    let mut wgsl_file = File::create(&path)
-        .await
-        .with_context(|| format!("Error opening {:?} for writing", &path))?;
-    wgsl_file
-        .write_all(frag_str.as_bytes())
-        .await
-        .with_context(|| format!("Error writing to {:?}", &path))?;
+    validate(&frag_str, &frag_path, "fragment").await?;
 
     //
     // Vertex Shader
@@ -120,15 +67,17 @@ pub async fn load_shaders(opts: FractalOpts) -> anyhow::Result<LoadedShaders> {
         })
         .context("Error loading vertex shader template")?;
 
-    info!("Writing Vertex Shader WGSL...");
-    let path = debug_dir().join("debug_vertex.wgsl");
-    let mut wgsl_file = File::create(&path)
+    info!("Writing vertex shader WGSL to debug file...");
+    let vert_path = debug_dir().join("debug_vertex.wgsl");
+    let mut wgsl_file = File::create(&vert_path)
         .await
-        .with_context(|| format!("Error opening {:?} for writing", &path))?;
+        .with_context(|| format!("Error opening {:?} for writing", &vert_path))?;
     wgsl_file
         .write_all(vert_str.as_bytes())
         .await
-        .with_context(|| format!("Error writing to {:?}", &path))?;
+        .with_context(|| format!("Error writing to {:?}", &vert_path))?;
+
+    validate(&vert_str, &vert_path, "vertex").await?;
 
     Ok(LoadedShaders {
         vertex: ShaderSource::Wgsl(Cow::Owned(vert_str)),
@@ -136,12 +85,35 @@ pub async fn load_shaders(opts: FractalOpts) -> anyhow::Result<LoadedShaders> {
     })
 }
 
-#[derive(Error, Debug)]
-pub enum ShaderError {
-    #[error("Missing template function '{}'", .0)]
-    MissingTemplateFunction(String),
-    #[error("Missing template constant '{}'", .0)]
-    MissingTemplateConstant(String),
-    #[error("Missing template type '{}'", .0)]
-    MissingTemplateType(String),
+async fn validate(source: &str, source_file: &Path, shader_name: &str) -> anyhow::Result<()> {
+    info!("Validating {} source...", shader_name);
+    let module = front::wgsl::parse_str(source)
+        .map_err(|e| {
+            anyhow!(
+                "Error in template file: {:?}\n{}",
+                source_file,
+                e.emit_to_string(source)
+            )
+        })
+        .with_context(|| format!("Error parsing {} shader", shader_name))?;
+
+    info!("Writing {} module as txt...", shader_name);
+    let path = debug_dir().join(format!("debug_{}.txt", shader_name));
+    let mut file = File::create(&path)
+        .await
+        .with_context(|| format!("Error opening {:?} for writing", &path))?;
+    file.write_all(format!("{:#?}", &module).as_bytes())
+        .await
+        .with_context(|| format!("Error writing to {:?}", &path))?;
+
+    info!("Validating {} module...", shader_name);
+    let mut validator = Validator::new(ValidationFlags::all(), Default::default());
+    let _ = validator
+        .validate(&module)
+        .with_context(|| format!("Error validating {} shader", shader_name))?;
+
+    Ok(())
 }
+
+#[derive(Error, Debug)]
+pub enum ShaderError {}
--- src/generator/gpu/shader/opts.rs
+++ src/generator/gpu/shader/opts.rs
@@ -1,384 +1,64 @@
-use std::collections::HashMap;
-
-use naga::{
-    Arena, ArraySize, BinaryOperator, Block, Constant, ConstantInner, Expression, Function, Handle,
-    MathFunction, Module, ScalarKind, ScalarValue, Span, Statement, TypeInner, VectorSize,
-};
+use liquid_core::{object, Object};
 
 use crate::generator::{
     args::{Multisampling, Smoothing},
     gpu::shader::ShaderError,
-    util::FloatKey,
     FractalOpts,
 };
-
-const SAMPLE_COUNT_NAME: &str = "t_sample_count";
-const SAMPLE_OFFSETS_NAME: &str = "t_sample_offsets";
-const SMOOTH_NAME: &str = "t_smooth";
-const LINEAR_INTERSECTION_NAME: &str = "linear_intersection";
 
 /// Structs implementing this trait can be used when generating fractals on the
 /// GPU.
 pub trait GpuFractalOpts {
-    fn install(&self, module: &mut Module) -> Result<(), ShaderError>;
+    fn globals(&self) -> Result<Object, ShaderError>;
 }
 
 impl GpuFractalOpts for FractalOpts {
-    fn install(&self, module: &mut Module) -> Result<(), ShaderError> {
-        self.smoothing.install(module)?;
-        self.multisampling.install(module)?;
-        Ok(())
+    fn globals(&self) -> Result<Object, ShaderError> {
+        let opts_obj = object!({
+            "c_real": self.c.re,
+            "c_imag": self.c.im,
+            "iterations": self.iterations,
+            "mandelbrot": self.mandelbrot,
+            "radius_squared": self.radius_squared,
+            "smoothing": self.smoothing.opts()?,
+            "multisampling": self.multisampling.opts()?,
+        });
+
+        Ok(object!({ "opts": opts_obj }))
     }
 }
 
 /// Structs implementing this trait can be used as smoothing options for
 /// generating fractals on the GPU.
 pub trait GpuSmoothing {
-    fn install(&self, module: &mut Module) -> Result<(), ShaderError>;
+    fn opts(&self) -> Result<Object, ShaderError>;
 }
 
 impl GpuSmoothing for Smoothing {
-    fn install(&self, module: &mut Module) -> Result<(), ShaderError> {
-        self.install_smoothing(module)?;
-        Ok(())
-    }
-}
-
-impl Smoothing {
-    fn install_smoothing(&self, module: &mut Module) -> Result<(), ShaderError> {
-        let handle = find_function_handle(&module.functions, SMOOTH_NAME)?;
-        let linear_intersection_handle =
-            find_function_handle(&module.functions, LINEAR_INTERSECTION_NAME)?;
-
-        let function = module.functions.get_mut(handle);
-        function.body = Block::new();
-        function.expressions.clear();
-        function.local_variables.clear();
-        function.named_expressions.clear();
-
-        let iterations_handle = function
-            .expressions
-            .append(Expression::FunctionArgument(0), Span::default());
-        let z_curr_handle = function
-            .expressions
-            .append(Expression::FunctionArgument(1), Span::default());
-        let z_prev_handle = function
-            .expressions
-            .append(Expression::FunctionArgument(2), Span::default());
-
-        match self {
-            Smoothing::None => {
-                let cast_handle_start = function.expressions.len();
-                let cast_handle = function.expressions.append(
-                    Expression::As {
-                        expr: iterations_handle,
-                        kind: ScalarKind::Float,
-                        convert: Some(4),
-                    },
-                    Span::default(),
-                );
-                let cast_handle_range = function.expressions.range_from(cast_handle_start);
-
-                function
-                    .body
-                    .extend(Some((Statement::Emit(cast_handle_range), Span::default())));
-
-                function.body.extend(Some((
-                    Statement::Return {
-                        value: Some(cast_handle),
-                    },
-                    Span::default(),
-                )));
-            },
-            Smoothing::LogarithmicDistance {
-                divisor, addend, ..
-            } => {
-                let divisor_constant = get_float_constant(&mut module.constants, *divisor);
-                let addend_constant = get_float_constant(&mut module.constants, *addend);
-
-                let divisor_handle = function
-                    .expressions
-                    .append(Expression::Constant(divisor_constant), Span::default());
-                let addend_handle = function
-                    .expressions
-                    .append(Expression::Constant(addend_constant), Span::default());
-
-                let range_start = function.expressions.len();
-                let cast_handle = function.expressions.append(
-                    Expression::As {
-                        expr: iterations_handle,
-                        kind: ScalarKind::Float,
-                        convert: Some(4),
-                    },
-                    Span::default(),
-                );
-                let length_handle = function.expressions.append(
-                    Expression::Math {
-                        fun: MathFunction::Dot,
-                        arg: z_curr_handle,
-                        arg1: Some(z_curr_handle),
-                        arg2: None,
-                        arg3: None,
-                    },
-                    Span::default(),
-                );
-                let log0_handle = function.expressions.append(
-                    Expression::Math {
-                        fun: MathFunction::Log,
-                        arg: length_handle,
-                        arg1: None,
-                        arg2: None,
-                        arg3: None,
-                    },
-                    Span::default(),
-                );
-                let log1_handle = function.expressions.append(
-                    Expression::Math {
-                        fun: MathFunction::Log,
-                        arg: log0_handle,
-                        arg1: None,
-                        arg2: None,
-                        arg3: None,
-                    },
-                    Span::default(),
-                );
-                let divide_handle = function.expressions.append(
-                    Expression::Binary {
-                        op: BinaryOperator::Divide,
-                        left: log1_handle,
-                        right: divisor_handle,
-                    },
-                    Span::default(),
-                );
-                let subtract_handle = function.expressions.append(
-                    Expression::Binary {
-                        op: BinaryOperator::Subtract,
-                        left: cast_handle,
-                        right: divide_handle,
-                    },
-                    Span::default(),
-                );
-                let add_handle = function.expressions.append(
-                    Expression::Binary {
-                        op: BinaryOperator::Add,
-                        left: subtract_handle,
-                        right: addend_handle,
-                    },
-                    Span::default(),
-                );
-                let range = function.expressions.range_from(range_start);
-
-                function
-                    .body
-                    .extend(Some((Statement::Emit(range), Span::default())));
-                function.body.extend(Some((
-                    Statement::Return {
-                        value: Some(add_handle),
-                    },
-                    Span::default(),
-                )));
-            },
-            Smoothing::LinearIntersection => {
-                let linear_intersection_call_handle = function.expressions.append(
-                    Expression::CallResult(linear_intersection_handle),
-                    Span::default(),
-                );
-
-                function.body.extend(Some((
-                    Statement::Call {
-                        function: linear_intersection_handle,
-                        arguments: vec![iterations_handle, z_curr_handle, z_prev_handle],
-                        result: Some(linear_intersection_call_handle),
-                    },
-                    Span::default(),
-                )));
-                function.body.extend(Some((
-                    Statement::Return {
-                        value: Some(linear_intersection_call_handle),
-                    },
-                    Span::default(),
-                )));
-            },
-        }
-
-        Ok(())
+    fn opts(&self) -> Result<Object, ShaderError> {
+        Ok(match self {
+            Smoothing::None => object!({ "kind": "none" }),
+            Smoothing::LogarithmicDistance { divisor, addend } => object!({
+                "kind": "log",
+                "divisor": divisor,
+                "addend": addend,
+            }),
+            Smoothing::LinearIntersection => object!({ "kind": "linear" }),
+        })
     }
 }
 
 /// Structs implementing this trait can be used as multisampling options for
 /// generating on the GPU.
 pub trait GpuMultisampling {
-    fn install(&self, module: &mut Module) -> Result<(), ShaderError>;
+    fn opts(&self) -> Result<Object, ShaderError>;
 }
 
 impl GpuMultisampling for Multisampling {
-    fn install(&self, module: &mut Module) -> Result<(), ShaderError> {
-        self.install_sample_count(module)?;
-        self.install_sample_offsets(module)?;
-        Ok(())
+    fn opts(&self) -> Result<Object, ShaderError> {
+        Ok(object!({
+            "sample_count": self.sample_count(),
+            "sample_offsets": self.offsets(),
+        }))
     }
-}
-
-impl Multisampling {
-    fn install_sample_count(&self, module: &mut Module) -> Result<(), ShaderError> {
-        replace_constant(
-            &mut module.constants,
-            SAMPLE_COUNT_NAME,
-            ConstantInner::Scalar {
-                width: 4,
-                value: ScalarValue::Uint(self.sample_count() as u64),
-            },
-        )
-    }
-
-    fn install_sample_offsets(&self, module: &mut Module) -> Result<(), ShaderError> {
-        let sample_count_handle = find_constant(&module.constants, SAMPLE_COUNT_NAME)?;
-        let vec2_f32_type_handle = module
-            .types
-            .iter()
-            .find(|(_handle, t)| {
-                t.inner
-                    == TypeInner::Vector {
-                        size: VectorSize::Bi,
-                        kind: ScalarKind::Float,
-                        width: 4,
-                    }
-            })
-            .ok_or_else(|| ShaderError::MissingTemplateType("vec2<f32>".to_string()))?
-            .0;
-        let sample_count_type_handle = module
-            .types
-            .iter()
-            .find(|(_handle, t)| {
-                t.inner
-                    == TypeInner::Array {
-                        base: vec2_f32_type_handle,
-                        size: ArraySize::Constant(sample_count_handle),
-                        stride: 8,
-                    }
-            })
-            .ok_or_else(|| {
-                ShaderError::MissingTemplateType("array<vec2<f32>, t_sample_count>".to_string())
-            })?
-            .0;
-
-        let handle = find_function_handle(&module.functions, SAMPLE_OFFSETS_NAME)?;
-
-        let function = module.functions.get_mut(handle);
-        function.body = Block::new();
-        function.expressions.clear();
-        function.local_variables.clear();
-        function.named_expressions.clear();
-
-        let mut constants = HashMap::new();
-
-        let offsets = self.offsets();
-        for offset in offsets.iter() {
-            let x_key = FloatKey::from_f32(offset.x);
-            let y_key = FloatKey::from_f32(offset.y);
-
-            if !constants.contains_key(&x_key) {
-                constants.insert(
-                    x_key,
-                    function.expressions.append(
-                        Expression::Constant(get_float_constant(&mut module.constants, offset.x)),
-                        Span::default(),
-                    ),
-                );
-            }
-
-            if !constants.contains_key(&y_key) {
-                constants.insert(
-                    y_key,
-                    function.expressions.append(
-                        Expression::Constant(get_float_constant(&mut module.constants, offset.y)),
-                        Span::default(),
-                    ),
-                );
-            }
-        }
-
-        let compose_start = function.expressions.len();
-        let mut vec_handles = vec![];
-        for offset in offsets {
-            let x_key = FloatKey::from_f32(offset.x);
-            let y_key = FloatKey::from_f32(offset.y);
-            vec_handles.push(function.expressions.append(
-                Expression::Compose {
-                    ty: vec2_f32_type_handle,
-                    components: vec![constants[&x_key], constants[&y_key]],
-                },
-                Span::default(),
-            ));
-        }
-
-        let final_handle = function.expressions.append(
-            Expression::Compose {
-                ty: sample_count_type_handle,
-                components: vec_handles,
-            },
-            Span::default(),
-        );
-        let compose_range = function.expressions.range_from(compose_start);
-
-        function
-            .body
-            .push(Statement::Emit(compose_range), Span::default());
-        function.body.push(
-            Statement::Return {
-                value: Some(final_handle),
-            },
-            Span::default(),
-        );
-
-        Ok(())
-    }
-}
-
-fn find_function_handle(
-    functions: &Arena<Function>,
-    name: &str,
-) -> Result<Handle<Function>, ShaderError> {
-    functions
-        .fetch_if(|f| match f.name {
-            None => false,
-            Some(ref fname) => fname == name,
-        })
-        .ok_or_else(|| ShaderError::MissingTemplateFunction(name.to_string()))
-}
-
-fn get_float_constant(constants: &mut Arena<Constant>, value: f32) -> Handle<Constant> {
-    constants.fetch_or_append(
-        Constant {
-            name: None,
-            specialization: None,
-            inner: ConstantInner::Scalar {
-                width: 4,
-                value: ScalarValue::Float(value as f64),
-            },
-        },
-        Span::default(),
-    )
-}
-
-fn replace_constant(
-    constants: &mut Arena<Constant>,
-    name: &str,
-    inner: ConstantInner,
-) -> Result<(), ShaderError> {
-    let handle = find_constant(constants, name)?;
-    let constant = constants.get_mut(handle);
-
-    constant.inner = inner;
-
-    Ok(())
-}
-
-fn find_constant(constants: &Arena<Constant>, name: &str) -> Result<Handle<Constant>, ShaderError> {
-    constants
-        .fetch_if(|c| match c.name {
-            None => false,
-            Some(ref const_name) => const_name == name,
-        })
-        .ok_or_else(|| ShaderError::MissingTemplateConstant(name.to_string()))
 }
