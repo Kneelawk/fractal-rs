fc4dbf93ed216a082972620b0694e77c118f57c4
Cyan Kneelawk <kneelawk@gmail.com>
2025-03-01 16:42:29 -0800

    Begin on fractal lang parser

--- crates/lang/src/parser/mod.rs
+++ crates/lang/src/parser/mod.rs
@@ -2,12 +2,12 @@
 
 mod lexer;
 
-use crate::ast::{AstConstant, AstExpression, AstExpressionImpl, AstProgram};
+use crate::ast::{AstConstant, AstExpression, AstExpressionImpl, AstProgram, BinaryOpType};
 use crate::parser::lexer::LexerToken;
+use chumsky::error;
 use chumsky::input::MapExtra;
-use chumsky::prelude::recursive;
-use chumsky::span::Span;
-use chumsky::{Parser, error, extra, select, text};
+use chumsky::prelude::*;
+use rug::Complex;
 use std::ops::Range;
 use std::sync::Arc;
 
@@ -63,17 +63,90 @@ fn mk_span<'a, 'b, 'src>(
     }
 }
 
-// fn expr_parser<'src>(
-//     prec: u32,
-// ) -> impl Parser<'src, LexerToken<'src>, AstExpression, ProgramExtra<'src>> {
-//     recursive(|expr| {
-//         let constant = select! {
-//             LexerToken::Boolean(b) => AstExpressionImpl::Constant(AstConstant::Boolean(b)),
-//             LexerToken::Integer(i) => AstExpressionImpl::Constant(AstConstant::Integer(i)),
-//             // LexerToken::
-//         };
-//     })
-// }
+fn expr_parser<'src>(
+    prec: u32,
+) -> impl Parser<'src, LexerToken<'src>, AstExpression, ProgramExtra<'src>> {
+    recursive(move |expr| {
+        let constant = select! {
+            LexerToken::Boolean(b) => AstExpressionImpl::Constant(AstConstant::Boolean(b)),
+            LexerToken::RealInteger(i) => AstExpressionImpl::Constant(AstConstant::Integer(i)),
+            LexerToken::RealNumber(n) => AstExpressionImpl::Constant(AstConstant::Complex(Complex::with_val(prec, (n, 0)))),
+            LexerToken::ImaginaryInteger(i) => AstExpressionImpl::Constant(AstConstant::Complex(Complex::with_val(prec, (0, i)))),
+            LexerToken::ImaginaryNumber(n) => AstExpressionImpl::Constant(AstConstant::Complex(Complex::with_val(prec, (0, n)))),
+            LexerToken::I => AstExpressionImpl::Constant(AstConstant::Complex(Complex::with_val(prec, (0, 1))))
+        }.labelled("value");
+
+        let ident = select! {LexerToken::Ident(s) => s}.labelled("identifier");
+
+        let items = expr
+            .clone()
+            .separated_by(just(LexerToken::Delim(',')))
+            .allow_trailing()
+            .collect::<Vec<_>>();
+
+        let let_ = just(LexerToken::Let)
+            .ignore_then(ident)
+            .then_ignore(just(LexerToken::Op("=")))
+            .then(expr.clone())
+            .map(|(name, value)| {
+                AstExpression::new(AstExpressionImpl::VarDeclareAssign {
+                    name: name.to_string(),
+                    assign: Box::new(value),
+                    mutable: false,
+                })
+            });
+
+        let parens = just(LexerToken::Delim('('))
+            .ignore_then(expr.clone())
+            .then_ignore(just(LexerToken::Delim(')')));
+
+        let pow = expr
+            .clone()
+            .then_ignore(just(LexerToken::Op("^")))
+            .then(expr.clone())
+            .map(|(left, right)| AstExpressionImpl::BinaryOp {
+                ty: BinaryOpType::Power,
+                left: Box::new(left),
+                right: Box::new(right),
+            });
+
+        let mul = expr
+            .clone()
+            .then(
+                just(LexerToken::Op("*"))
+                    .to(BinaryOpType::Times)
+                    .or(just(LexerToken::Op("/")).to(BinaryOpType::Divide)),
+            )
+            .then(expr.clone())
+            .map(|((a, op), b)| {
+                AstExpression::new(AstExpressionImpl::BinaryOp {
+                    left: Box::new(a),
+                    ty: op,
+                    right: Box::new(b),
+                })
+            });
+
+        let add = expr
+            .clone()
+            .then(
+                just(LexerToken::Op("+"))
+                    .to(BinaryOpType::Plus)
+                    .or(just(LexerToken::Op("-")).to(BinaryOpType::Minus)),
+            )
+            .then(expr.clone())
+            .map(|((a, op), b)| {
+                AstExpression::new(AstExpressionImpl::BinaryOp {
+                    left: Box::new(a),
+                    ty: op,
+                    right: Box::new(b),
+                })
+            });
+
+        todo!()
+    });
+
+    todo!()
+}
 //
 // fn parser<'src>(prec: u32) -> impl Parser<'src, LexerToken<'src>, AstProgram, ProgramExtra<'src>> {}
 //
